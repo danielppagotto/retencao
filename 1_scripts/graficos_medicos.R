20ef93cf0dbd0d65c325539a33d156dd7fb855b6
Daniel Pagotto <danielppagotto@gmail.com>
2024-07-16 14:20:20 -0300

    update

--- 1_scripts/graficos_medicos.R
+++ 1_scripts/graficos_medicos.R
@@ -1,7 +1,9 @@
 
 ## Script para elaboração de figuras do artigo "Como mensurar a retenção de profissionais médicos pelas regiões de saúde no Brasil"
 
-# carregando pacotes
+
+# Carregando Pacotes ------------------------------------------------------
+
 library(tidyverse)
 library(readxl)
 library(geojsonio)
@@ -9,60 +11,88 @@ library(RODBC)
 library(tmap)
 library(ggspatial)
 library(ggrepel)
+library(sf)
+
+# Importando dados --------------------------------------------------------
+
+# Dados de retencao
+Medico_dfs_geral <- 
+  read_delim("0_dados/Medico_retencao_geral.csv", 
+             delim = ";", 
+             escape_double = FALSE, 
+             trim_ws = TRUE) |> 
+  select(-`...1`) |> 
+  mutate(regiao_saude = as.character(regiao_saude))
+
+# Dados da hierarquia
+
+hierarquia_completa <- 
+  read_csv("0_dados/hierarquia_atualizada.csv") 
 
 
-# Importando dados
+hierarquia_atualizada <- 
+  read_csv("0_dados/hierarquia_atualizada.csv") |> 
+      select(regiao, cod_uf, uf, 
+             cod_regsaud, regiao_saude) |> 
+      mutate(cod_regsaud = as.character(cod_regsaud)) |> 
+      distinct(regiao, cod_uf, uf, 
+               cod_regsaud, regiao_saude)
 
-#Medico_dfs_geral <- read_excel("0_dados/Medico_dfs_geral.xlsx")
+# juntando as bases
 
-Medico_dfs_geral <- read_delim("0_dados/Medico_retencao_geral.csv", 
-                               delim = ";", 
-                               escape_double = FALSE, 
-                               trim_ws = TRUE) |> 
-                    select(-`...1`) |> 
-                    mutate(regiao_saude = as.character(regiao_saude))
+Medico_dfs_geral <- 
+          Medico_dfs_geral |> 
+              left_join(hierarquia_atualizada, 
+                        by = c("regiao_saude"="cod_regsaud")) |> 
+              rename(cod_regiao_saude = regiao_saude,
+                     nome_regiao_saude = regiao_saude.y)
 
-# Pegando dados da hierarquia
+# Analises ----------------------------------------------------------------
+# 1) Boxplot por regiao ------------------------------------------------------
 
-hierarquia_completa <- read_csv("0_dados/hierarquia_atualizada.csv") 
-
-
-hierarquia_atualizada <- read_csv("0_dados/hierarquia_atualizada.csv") |> 
-                              select(regiao, cod_uf, uf, cod_regsaud, regiao_saude) |> 
-                              mutate(cod_regsaud = as.character(cod_regsaud)) |> 
-                              distinct(regiao, cod_uf, uf, cod_regsaud, regiao_saude)
-
-# juntando tudo 
-
-Medico_dfs_geral <- Medico_dfs_geral |> 
-                      left_join(hierarquia_atualizada, 
-                                by = c("regiao_saude"="cod_regsaud")) |> 
-                      rename(cod_regiao_saude = regiao_saude,
-                             nome_regiao_saude = regiao_saude.y)
-
-
-# Construindo boxplot por região
+medianas_regiao <- Medico_dfs_geral %>%
+  rename(Região = regiao) %>%
+  mutate(Região = str_replace(Região, "^Região ", "")) %>%
+  group_by(Região) %>%
+  summarize(mediana = median(retencao_geral, na.rm = TRUE)) %>%
+  ungroup()
 
 Medico_dfs_geral |> 
-  rename(Região = regiao) |> 
+  rename(Região = regiao) |>
+  mutate(Região = str_replace(Região, "^Região ", "")) |> 
   ggplot(aes(x= fct_reorder(Região, retencao_geral, 
-                           .desc = TRUE), y=retencao_geral, 
-             fill=Região) , las=5) +
-    geom_boxplot() + theme_minimal() + xlab("Região") + 
+                           .desc = TRUE), y=retencao_geral), 
+         las=5) +
+    geom_boxplot(aes(fill=Região)) + theme_minimal() + xlab("Região") + 
     ylab("Taxa de retenção") + 
+    scale_y_continuous(limits = c(0, 1), 
+                       breaks = seq(0, 1, by = 0.25)) + 
+  
     theme(
       axis.text.x = element_text(size = 16),  
       axis.text.y = element_text(size = 16),
       legend.text = element_text(size = 16),
       axis.title.x = element_text(size = 16),
-      axis.title.y = element_text(size = 16)
-    )
+      axis.title.y = element_text(size = 16),
+      legend.position = "none"
+    ) +
+  geom_text(data = medianas_regiao, 
+            aes(x = Região, y = mediana, label = round(mediana, 2)),
+            position = position_nudge(x = 0.2, 
+                                      y = -0.02), 
+            size = 5)
 
 # Calculando medidas resumo da variável "retencao_geral" por Região
 
-tapply(Medico_dfs_geral$retencao_geral, Medico_dfs_geral$regiao, summary)
+media_uf <- 
+  Medico_dfs_geral |> 
+  group_by(uf) |> 
+  summarise(media = mean(retencao_geral))
 
-# Construindo boxplot por UF
+
+mean(Medico_dfs_geral$retencao_geral)
+
+# Construindo boxplot por UF ----------------------------------------------
 
 Medico_dfs_geral <- Medico_dfs_geral |> 
                       mutate(regiao_order = 
@@ -74,74 +104,95 @@ Medico_dfs_geral <- Medico_dfs_geral |>
 
 # por uf
 
-Medico_dfs_geral |>
+median(Medico_dfs_geral$retencao_geral)
+
+# Calculando a mediana para cada grupo
+medianas <- Medico_dfs_geral %>%
+  rename(Região = regiao) %>%
+  group_by(uf, Região) %>%
+  summarize(mediana = median(retencao_geral), .groups = 'drop') |> 
+  filter(uf != "Distrito Federal")
+
+# Criando o gráfico por UF
+Medicos_regioes <- 
+  Medico_dfs_geral |>
   rename(Região = regiao) |>
+  mutate(Região = str_replace(Região, "^Região ", "")) 
+
+Medicos_regioes |> 
+  filter(uf != "Distrito Federal") |> 
   ggplot(aes(x = fct_reorder(uf, regiao_order, .desc = TRUE), 
-             y = retencao_geral, fill = Região)) +
-  geom_boxplot() +
+             y = retencao_geral)) +
+  geom_boxplot(aes(fill = Região)) +
   coord_flip() +
-  geom_hline(yintercept = 0.512, linetype = "dashed", color = "red") +
+  geom_hline(yintercept = 0.510, 
+             linetype = "dashed", 
+             color = "red") +
   theme_minimal() +
   xlab("UF") +
-  ylab("Retenção") +
+  ylab("Taxa de Retenção") +
+  scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, by = 0.25)) + 
+  geom_text(data = medianas, aes(x = uf, y = mediana, label = round(mediana, 2)), 
+            hjust = -0.3, size = 4) +  # Ajuste hjust e size conforme necessário
   theme(
-    axis.text.x = element_text(size = 12),  
-    axis.text.y = element_text(size = 12)   
+    axis.text.x = element_text(size = 16),  
+    axis.text.y = element_text(size = 16),
+    legend.position = "bottom"
   )
 
-## Gráfico de retenção vs oferta 
+
+# Retencao vs densidade ---------------------------------------------------
+
 
 retencao_uf <- Medico_dfs_geral |> 
-                    group_by(cod_uf, regiao) |> 
+                    group_by(cod_uf, uf, regiao) |> 
                     summarise(media_retencao = mean(retencao_geral))
 
 razao <- read_excel("0_dados/razao_medicos.xlsx")
 
 
 tbl_uf <- razao |> 
-  inner_join(retencao_uf, by = "cod_uf") 
+  inner_join(retencao_uf, by = "cod_uf")
 
+# Calcular o coeficiente de correlação e o valor p
+cor_test <- cor.test(tbl_uf$media_retencao, tbl_uf$Razão)
+
+# Extraindo o coeficiente de correlação e o valor p
+r <- round(cor_test$estimate, 3)
+p_value <- cor_test$p.value
+p_text <- ifelse(p_value < 0.01, "p < 0.01", paste("p =", round(p_value, 3)))
+
+# Criar o gráfico com a anotação do coeficiente de correlação
 tbl_uf |> 
   rename(Região = regiao) |> 
+  mutate(Região = str_replace(Região, "^Região ", "")) |> 
   ggplot(aes(x = media_retencao, y = Razão)) + 
   geom_point() + 
   geom_label(aes(label = UF, fill = Região)) + 
   geom_smooth(method = "lm", se = FALSE) + 
   theme_minimal() + 
   xlab("Retenção") + 
-  ylab("Médicos por 1000 habitantes") +
+  ylab("Razão de médicos por 1000 habitantes") +
+  scale_x_continuous(limits = c(0, 1)) +  
+  scale_y_continuous(limits = c(0, 7)) +  
   theme(
     text = element_text(size = 16),          
     axis.title = element_text(size = 14),    
     axis.text = element_text(size = 14),     
     legend.title = element_text(size = 16),  
-    legend.text = element_text(size = 14)    
-  )
-
-cor(tbl_uf$Razão, tbl_uf$media_retencao)
-
-
-modelo <- lm(Razão ~ media_retencao, tbl_uf)
-
-summary(modelo)
+    legend.text = element_text(size = 14),
+    legend.position = "bottom"
+  ) +
+  annotate("text", x = 0.8, y = 6.5, 
+           label = paste("r =", r, ",", p_text),
+           size = 6, hjust = 1)
 
 
-# Criando mapa de regiões de saúde considerando os percentis de retenção. 
+r <- cor.test(tbl_uf$Razão, tbl_uf$media_retencao)
+r
 
-# preparando os shapes dos mapas
-library(sf)
-library(ggplot2)
-library(ggrepel)
-library(ggspatial)
-library(dplyr)
-library(geojsonio)
 
-data("World", package = "rnaturalearth")
-
-latam <- World |> filter(continent == "South America")
-
-# Converter para sf
-latam <- sf::st_as_sf(latam)
+# Mapa de regiões de saúde ------------------------------------------------
 
 # Carregar shapefile GeoJSON e converter para sf
 spdf <- geojson_read("1_scripts/shape file regioes saude.json", what = "sp")
@@ -190,4 +241,3 @@ mapa +
     style = ggspatial::north_arrow_nautical(
       fill = c("grey40", "white"),
       line_col = "grey20"))
-
