4db3a3f9142668bfbdaa63b762a782150356ae78
Daniel Pagotto <danielppagotto@gmail.com>
2024-07-19 11:05:31 -0300

    update

--- 1_scripts/graficos_retencao_dispersao.R
+++ 1_scripts/graficos_retencao_dispersao.R
@@ -3,10 +3,11 @@ library(tidyverse)
 library(ggplot2)
 library(GGally)
 
-data_retencao <- read_excel("0_dados/retencao_geral_completo.xlsx")
+data_retencao <- read_excel("~/GitHub/retencao/0_dados/retencao_geral_completo.xlsx")
 
 #Gerando gráfico
-ggpairs(data_retencao,
+grafico <- 
+  ggpairs(data_retencao,
         columns = c("retencao_medicos", "retencao_enfermeiros", 
                     "retencao_dentistas", "retencao_tec_aux_enf"),
         upper = list(continuous = "cor"),
@@ -14,4 +15,9 @@ ggpairs(data_retencao,
         mapping = aes(color = regiao),
         title = "Gráficos de Dispersão: Taxas de Retenção por Ocupação",
         xlab = "Taxa de Retenção",
-        ylab = "Taxa de Retenção")
+        ylab = "Taxa de Retenção") + theme_minimal()
+
+grafico
+
+ggsave(grafico, filename = "grafico.jpeg",
+       width = 3000, height = 3000, units = "px", dpi = 300)
